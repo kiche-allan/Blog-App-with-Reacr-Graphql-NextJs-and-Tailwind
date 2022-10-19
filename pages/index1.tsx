297d6f3fea7cabd900ce9dfeb92e9556dc310df4
allan Kiche <allankiche18@gmail.com>
2022-10-19 17:04:18 +0300

    first commit

---
+++ pages/index1.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+const posts = [
+  {title: 'React Testing', excerpt: 'This is a post about React Testing', id: '1'},
+  {title: 'React with Tailwinf', excerpt: 'This is a post about React and Tailwind', id: '2'}
+]
+
+const Home: NextPage = () => {
+  return ( 
+    <>
+    <div className="container mx-auto px-10 mb-8 ">
+      <Head>
+        <title>CMS blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+      <div className="lg:col-span-8 col-span-1">
+      {posts.map((post) => <PostCard post={post}  key ={post.title}/>)}
+      
+       </div>
+      <div className="lg:col-span-4 col-span-1">
+        <div className="lg:sticky relative top-8">
+
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
